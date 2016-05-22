7129556b5633f2369cc53666d50ac6f0865b0f8c
margeir <margeir@met.is>
2016-05-22 08:34:38 +0000

    Búinn að bæta við því sem var komið í kerfið.

--- Isfelag.py
+++ Isfelag.py
@@ -9,15 +9,34 @@ from System import Random
 
 
 trace.Put("Running script for calculating pallet weight")
-#Það sem vantar er að geta bail þegar brettið er þyngra en það sem verið er að vigta.
+
 
 if context.CurrentPack.Record.Pallet == None:
-	trace.Put("No Pallet for pack")
+	trace.Put("No Pallet for pack in context")
 else:
-	trace.Put("Pack has Pallet")
-	if context.CurrentPallet.Record.Weight > 0:
-		trace.Put("Brettið er með þyngd á sér {0}",context.CurrentPallet.Weight)
-		context.CurrentPack.Record.Weight = (context.CurrentPack.Record.Weight) - (context.CurrentPallet.Weight)
-		trace.Put("Ný þyngd á pakka er {0}", context.CurrentPack.Weight)
+	trace.Put("Pack has Pallet in context")
+
+	if context.CurrentPack.Record.Weight < context.CurrentPallet.Record.Weight:
+		if context.CurrentPallet.Record.Weight > 0:
+			trace.Put("Pallet has more than 0 kg. so update required, weight on pallet with new pack Kg. {0}",context.CurrentPallet.Record.Weight)
+			brettif = context.CurrentPack.Record.Weight - context.CurrentPallet.Record.Weight
+			##New pack must be more weight than the pallet if we are adding to the pallet.
+			##brettif = context.CurrentPallet.Record.Weight - context.CurrentPack.Record.Weight
+			trace.Put("Start weight of pallet Kg. {0}",brettif)
+			pakkie = (context.CurrentPack.Record.Weight) - (brettif)
+			trace.Put("Weight of the pack we are going to add Kg. {0}",pakkie)
+			##Breytum þyngd pakka í það sem verið er að bæta við; set the weight record to what we are adding to the pallet.
+			context.CurrentPack.Record.Weight = pakkie
+		
+			trace.Put("Pack weight added to the pallet")
+			context.CurrentPallet.Record.Weight = brettif + pakkie
+			context.CurrentPallet.Record.CurAmount = brettif + pakkie
+		
+		else:
+			trace.Put("The pallet is 0 or less weight")
+			##Svo við bætum einfaldlega þynd pakka við bretið, so we add the pack weight to the pallet.
 	else:
-		trace.Put("Ekki 1 pakki á bretti!!!!")
+		trace.Put("Total pack weight can not be less than pallet, we are adding to the pallet.")
+		#Vantar eitthvað hér til þess að stopa keyrsluna á unitOp Margeir
+		#raise Exception("Getur ekki verið minna en bretti")
+		
